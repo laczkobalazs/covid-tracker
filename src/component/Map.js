0efe2eee6eb6d209eadee23ff892dca06f9342d9
laczkobalazs <laczko.balazs89@gmail.com>
2020-12-17 21:05:39 +0100

    adds further attributes for map

--- src/component/Map.js
+++ src/component/Map.js
@@ -6,6 +6,7 @@ function Map() {
     <div className="map">
       <LeafletMap >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       </LeafletMap>
     </div>
   )
